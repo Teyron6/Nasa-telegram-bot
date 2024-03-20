2140781a49e82d4597d55259ff6a6c4f92894a06
Teyron6 <glushakov.tema227@mail.ru>
2024-03-20 19:45:09 +0500

    Вынес id запуска в аргументы запуска скрипта в spacex.py.

--- spacex.py
+++ spacex.py
@@ -1,22 +1,31 @@
 import requests
 from download_img import download_img
+import random
+import argparse
 
 
-def fetch_spacex_last_launch():
-    url = 'https://api.spacexdata.com/v5/launches/5eb87d42ffd86e000604b384'
-    response = requests.get(url)
+def fetch_spacex_last_launch(launch_id):
+    link = f'https://api.spacexdata.com/v5/launches/{launch_id}'
+    response = requests.get(link)
     response.raise_for_status()
     urls = response.json()['links']['flickr']['original']
-    n = 1
-    for i in urls:
-        filename = f'spacex/spacex_{n}.jpeg'
-        url = i
-        n += 1
-        download_img(url, filename)
+    for url_num, url in enumerate(urls):
+        path = f'spacex/spacex_{url_num}.jpeg'
+        download_img(url, path)
 
 
 def main():
-    fetch_spacex_last_launch()
+    parser = argparse.ArgumentParser(
+        description='Эта программа позволит вам загрузить фотографии с запуска SpaceX.'
+    )
+    parser.add_argument(
+        '--id',
+        dest='launch_id',
+        default='5eb87d47ffd86e000604b38a',
+        help='Укажите ID запуска SpaceX, с которого можно загрузить фотографии.'
+    )
+    args = parser.parse_args()
+    fetch_spacex_last_launch(args.launch_id)
 
 
 if __name__ == '__main__':
